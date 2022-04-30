1aa746e98611784aad8a349f6cb3388476bf6506
PaolaP <paolaperez8@gmail.com>
2022-04-29 23:42:31 -0400

    add header component with fragment

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import  { Fragment } from 'react';
+
+const Header = () => {
+  return (
+    <Fragment>
+      <h2>NFT art collection for wanderlusts</h2></Fragment>
+  );
+};
+
+export default Header;
