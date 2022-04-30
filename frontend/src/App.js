3c898a50eef440da8eb1edf49209708db8f0573f
PaolaP <paolaperez8@gmail.com>
2022-04-30 01:13:33 -0400

    add buy btn to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Buy from './components/Buy';
 
 function App() {
   return (
     <div className="App">
       <h1>globeART</h1>
       <Header className="" name="Julian" message="NFT art collection for wanderlusts" />
+      <Buy></Buy>
     </div>
   );
 }
