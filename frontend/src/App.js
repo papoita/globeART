4db759a35be9178374b1c3dc01efe235f5da7b0e
PaolaP <paolaperez8@gmail.com>
2022-05-01 18:16:48 -0400

    add navbar component to src

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import SceneView from "./components/SceneView";
 import Header from './components/Header';
 import Buy from './components/Buy';
+import Navbar from './components/Navbar'
 
 import esriConfig from "@arcgis/core/config.js";
 esriConfig.assetsPath = "./assets";
@@ -12,6 +13,7 @@ esriConfig.assetsPath = "./assets";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>globeART</h1>
       <SceneView />
       <Header className="" name="Julian" message="NFT art collection for wanderlusts" />
