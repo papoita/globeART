901fe82620a3b1207b9b278e5da564ccb298da88
Martha Staus <mjstaus@gmail.com>
2022-05-02 07:15:00 -0700

    FEATURE: Change super to ERC721URIStorage (prev ERC721)

--- contracts/GlobeArtNFT.sol
+++ contracts/GlobeArtNFT.sol
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 /// @custom:security-contact <emergency contact email here>
-contract GlobeArtNFT is ERC721, Ownable {
+contract GlobeArtNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("globeArtNFT", "GAN") {}
+    constructor() ERC721("globeArtNFT", "GANFT") {}
 
-    function createGlobeArtNFT(string memory tokenURI) external onlyOwner {
+    function createGlobeArtNFT(string memory tokenURI) external onlyOwner returns(uint) {
        uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(tokenId, tokenURI);
         return(tokenId);
     }
 
