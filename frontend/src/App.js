01843b83b43f5c32975111007c424d685a070af4
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-05-01 20:37:45 -0400

    build: render the location in App component for visibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,33 @@
 //import logo from './logo.svg';
 import React from "react";
-import './App.css';
+import "./App.css";
 
 import SceneView from "./components/SceneView";
-import Header from './components/Header';
-import Buy from './components/Buy';
+import Header from "./components/Header";
+import Buy from "./components/Buy";
+import useGeolocation from "./hooks/useGeolocation";
 
 import esriConfig from "@arcgis/core/config.js";
 esriConfig.assetsPath = "./assets";
 
 function App() {
+  const location = useGeolocation();
+
   return (
     <div className="App">
       <h1>globeART</h1>
       <SceneView />
-      <Header className="" name="Julian" message="NFT art collection for wanderlusts" />
+      <Header
+        className=""
+        name="Julian"
+        message="NFT art collection for wanderlusts"
+      />
       <Buy></Buy>
-      
+      <div>
+        {location.displayLocation ? JSON.stringify(location) : "Loading"}
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-
-
