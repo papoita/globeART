ec6dcd2f3edade20a1310780c6f2cdac71ab82d6
Martha Staus <mjstaus@gmail.com>
2022-05-02 07:27:04 -0700

    FEATURE: Add saveFrontEndFiles function to deployment script - saves deployed contract address and artifact as JSON files in frontend/contractsData on deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,6 +6,30 @@ async function main() {
   await globeArtNFT.deployed();
 
   console.log("GlobeArtNFT deployed to:", globeArtNFT.address);
+
+   // Save copies of each contracts abi and address to the frontend.
+   saveFrontendFiles(globeArtNFT, "GlobeArtNFT");
+ }
+ 
+ function saveFrontendFiles(contract, name) {
+   const fs = require("fs");
+   const contractsDir = __dirname + "/../frontend/contractsData";
+ 
+   if (!fs.existsSync(contractsDir)) {
+     fs.mkdirSync(contractsDir);
+   }
+ 
+   fs.writeFileSync(
+     contractsDir + `/${name}-address.json`,
+     JSON.stringify({ address: contract.address }, undefined, 2)
+   );
+ 
+   const contractArtifact = artifacts.readArtifactSync(name);
+ 
+   fs.writeFileSync(
+     contractsDir + `/${name}.json`,
+     JSON.stringify(contractArtifact, null, 2)
+   );
 }
 
 main()
