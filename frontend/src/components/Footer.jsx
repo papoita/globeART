3e8ab601fbad29129ecf4f92f55342b18bb8037f
PaolaP <paolaperez8@gmail.com>
2022-05-03 13:25:58 -0400

    feature: add footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <Card className="text-center">
+      <Card.Header>Contact Us</Card.Header>
+      
+      <Card.Footer className="text-muted">links to github, twitter, linkedin, fb</Card.Footer>
+    </Card>
+  );
+}
+
+export default Footer;
