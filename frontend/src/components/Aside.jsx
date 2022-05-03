2441a36a24b52317d61591fff1dbf6b39e076eda
PaolaP <paolaperez8@gmail.com>
2022-05-03 19:46:52 -0400

    feature: add route to nft collection, clean up

--- frontend/src/components/Aside.jsx
+++ frontend/src/components/Aside.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Aside = () => {
   return (
-    <Card border="primary" style={{ height: 700 }}>
-      <Card.Img variant="top" src="./name.png" />
+    <Card border="primary" style={{ height: 780 }}>
+      <Card.Img variant="top" src="./samplenft.png" />
       <Card.Body variant="dark">
-        <Card.Title> <h1>NFT art collection for wanderlusts</h1></Card.Title>
+        <Card.Title>
+          {" "}
+          <h1>globeART</h1>
+        </Card.Title>
         <Card.Text>
-          <p>
-            Do you love travelling and art, but tired of collecting fridge
-            magnets?
-          </p>
-          <p>
-            This beautiful NFT artwork only obtainable when you visit the city,
-            will remind you of the places that you have been and the memories
-            that you have.
-          </p>
-          <p> The more you travel, the more you can collect.</p>
+          <br />
+          <br />
+          <h2>NFT art collection for wanderlusts</h2>
+          <br />
+          Do you love travelling and art, but tired of collecting fridge
+          magnets?
+          <br />
+          <br />
+          This beautiful NFT artwork only obtainable when you visit the city,
+          will remind you of the places that you have been and the memories that
+          you have.
+          <br />
+          <br />
+          The more you travel, the more you can collect.
         </Card.Text>
-        <Button gb="primary">Start your Collection</Button>
+        <Button gb="primary" as={Link} to="/nftcollection">
+          Start your Collection
+        </Button>
       </Card.Body>
     </Card>
   );
