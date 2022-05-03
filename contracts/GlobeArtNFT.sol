6ec2d27c0179f26a8f7aecdad5f2bdff46172f43
Martha Staus <mjstaus@gmail.com>
2022-05-02 19:06:41 -0700

    FEATURE: Add nftsCollections mapping, add parameter to createGlobeArtsNFT method for passing in collection name

--- contracts/GlobeArtNFT.sol
+++ contracts/GlobeArtNFT.sol
@@ -3,22 +3,20 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 /// @custom:security-contact <emergency contact email here>
 contract GlobeArtNFT is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
-
+ 
+    mapping(uint256=> string) public nftsCollections;
+    uint public tokenCount;
+   
     constructor() ERC721("globeArtNFT", "GANFT") {}
 
-    function createGlobeArtNFT(string memory tokenURI) external onlyOwner returns(uint) {
-       uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        return(tokenId);
+    function createGlobeArtNFT(string memory tokenURI, string memory collectionName) external onlyOwner returns(uint) {
+        tokenCount ++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, tokenURI);
+        nftsCollections[tokenCount]=collectionName;
+        return(tokenCount);
     }
-
 }
