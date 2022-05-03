62a75b42b9c374926f62a87a1321abaee98ab4d9
PaolaP <paolaperez8@gmail.com>
2022-05-03 13:25:04 -0400

    feature: add homepage view

--- frontend/src/views/Homepage.jsx
+++ frontend/src/views/Homepage.jsx
@@ -1,1 +1,29 @@
-// this unifies several components, 
+// this unifies several components,
+import React from "react";
+import "../App.css";
+import { Row, Col } from "react-bootstrap";
+
+import SceneView from "../components/SceneView";
+import Navigation from "../components/Navigation";
+import Aside from "../components/Aside";
+import Footer from "../components/Footer";
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <Navigation />
+      <Row>
+        <Col sm={8}><SceneView /></Col>
+        
+        <Col sm={4}><Aside /></Col>
+        
+      </Row>
+      <Footer />
+
+      
+      
+    </div>
+  );
+}
+
+export default Homepage;
