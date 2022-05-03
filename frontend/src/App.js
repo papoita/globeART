6d7cd51c6ca849e2451585f0e3c218a8e3ae5c2a
PaolaP <paolaperez8@gmail.com>
2022-05-03 13:27:22 -0400

    chore: remove hp components from app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,13 @@
 import React from "react";
 import "./App.css";
 
-import SceneView from "./components/SceneView";
-import Header from "./components/Header";
+//import SceneView from "./components/SceneView";
+//import Header from "./components/Header";
 import Buy from "./components/Buy";
 import useGeolocation from "./hooks/useGeolocation";
-import Navigation from "./components/Navigation";
+//import Navigation from "./components/Navigation";
+//import Aside from "./components/Aside";
+import Homepage from "./views/Homepage";
 
 import esriConfig from "@arcgis/core/config.js";
 esriConfig.assetsPath = "./assets";
@@ -20,14 +22,12 @@ function App() {
 
   return (
     <div className="App">
-      <Navigation />
-      
-      <SceneView />
-      <Header
+      < Homepage />
+      {/* <Header
         className=""
         name="Julian"
         message="NFT art collection for wanderlusts"
-      />
+      /> */}
       <Buy></Buy>
       <div>
         {location.displayLocation ? JSON.stringify(location) : "Loading"}
