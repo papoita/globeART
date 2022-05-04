d2a2dfa5af451fe73be1a2099de027165d981a42
PaolaP <paolaperez8@gmail.com>
2022-05-04 15:13:07 -0400

    feature: remove button allow, bring in geolocation hook, add allow location setter

--- frontend/src/components/Location.jsx
+++ frontend/src/components/Location.jsx
@@ -1,29 +1,26 @@
 import React from "react";
-import { useState } from "react";
 import { Button, Alert } from "react-bootstrap";
-import { useGeolocation } from "../hooks/useGeolocation";
+import useGeolocation from "../hooks/useGeolocation";
 
 function Location() {
-  const [show, setShow] = useState(true);
+  const { allow, setAllow } = useGeolocation();
 
   return (
     <>
-      <Alert bg="warning" variant="warning" show={show}>
+      <Alert bg="warning" variant="warning" show={!allow}>
         <Alert.Heading>Allow Location</Alert.Heading>
         <p>
           In order to collect NFTs you have to physically be in the location.
           Share your location to start collecting!
         </p>
-        <useGeolocation />
+
         <hr />
         <div className="d-flex justify-content-end">
-          <Button onClick={() => setShow(false)} variant="outline-warning">
+          <Button onClick={() => setAllow(true)} variant="outline-warning">
             Allow
           </Button>
         </div>
       </Alert>
-
-      {!show && <Button onClick={() => setShow(true)}>Share Location</Button>}
     </>
   );
 }
