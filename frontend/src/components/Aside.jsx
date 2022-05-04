c4617cd6377c0a02fc58a68d7df45463dc366dc3
PaolaP <paolaperez8@gmail.com>
2022-05-04 15:11:34 -0400

    feature: improve style for logo and description of app

--- frontend/src/components/Aside.jsx
+++ frontend/src/components/Aside.jsx
@@ -4,27 +4,22 @@ import { Link } from "react-router-dom";
 
 const Aside = () => {
   return (
-    <Card border="primary" style={{ height: 780 }}>
-      <Card.Img variant="top" src="./samplenft.png" />
+    <Card className="rounded-0" border="primary" style={{ height: 780 }}>
+      <Card.Img className="rounded-0" variant="top" src="./samplenft.png" />
       <Card.Body variant="dark">
-        <Card.Img style={{ width: "20rem" }} src="./logonamesm.png"></Card.Img>
-        <Card.Title></Card.Title>
-        <Card.Text>
-          <br />
-          <br />
-          <h2>NFT art collection for wanderlusts</h2>
-          <br />
+        <Card.Img  src="./logonamesm.png"></Card.Img>
+        <Card.Title className="fs-1 fw-bold">NFT art collection for wanderlusts</Card.Title>
+        <Card.Text className="p-2">
           Do you love travelling and art, but tired of collecting fridge
           magnets?
-          <br />
-          <br />
+        </Card.Text>
+        <Card.Text className="p-2">
           This beautiful NFT artwork only obtainable when you visit the city,
           will remind you of the places that you have been and the memories that
           you have.
-          <br />
-          <br />
-          The more you travel, the more you can collect.
         </Card.Text>
+        <Card.Text className="p-2">The more you travel, the more you can collect.</Card.Text>
+
         <Button gb="primary" as={Link} to="/nftcollection">
           Start your Collection
         </Button>
