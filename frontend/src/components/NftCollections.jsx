f3a7cba9866d39862304897b5aeb7224afdf7db1
PaolaP <paolaperez8@gmail.com>
2022-05-04 15:14:13 -0400

    feature: fix image display

--- frontend/src/components/NftCollections.jsx
+++ frontend/src/components/NftCollections.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 
-import { Carousel } from "react-bootstrap";
+import { Carousel, Card, Button } from "react-bootstrap";
 import Navigation from "./Navigation";
 import Buy from "./Buy";
 
-function Nft_collections() {
+function NftCollections() {
   return (
     <>
       <Navigation />
-      <Carousel fade>
-        <Carousel.Item>
-          <img className="d-block w-100" src="athens.png" alt="First slide" />
+      <Carousel className="m-5">
+        <Carousel.Item >
+        <img className="d-block w-100" src="athens.png" alt="Third slide" />
+
           <Carousel.Caption>
-            <h3>Athens 2022</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3> Athens 2022</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
             <Buy />
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item style={{ width: "100vw", height: "100vh" }}>
           <img className="d-block w-100" src="berlin.png" alt="Second slide" />
 
           <Carousel.Caption>
@@ -26,7 +29,7 @@ function Nft_collections() {
             <Buy />
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item $carousel-control-width>
           <img className="d-block w-100" src="cairo.png" alt="Third slide" />
 
           <Carousel.Caption>
@@ -36,8 +39,8 @@ function Nft_collections() {
             </p>
             <Buy />
           </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
+        </Carousel.Item >
+        <Carousel.Item >
           <img className="d-block w-100" src="lisbon.png" alt="Third slide" />
 
           <Carousel.Caption>
@@ -53,4 +56,4 @@ function Nft_collections() {
   );
 }
 
-export default Nft_collections;
+export default NftCollections;
