d228a7a729b674b5560078e9199cb7385ff93a87
PaolaP <paolaperez8@gmail.com>
2022-05-04 22:50:30 -0400

    feature:place butto in card for better styling in view

--- frontend/src/components/Buy.jsx
+++ frontend/src/components/Buy.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 // let count = 0;
 
@@ -17,13 +17,15 @@ const Buy = () => {
   };
 
   return (
-    <>
-      <Button gb="primary" onClick={increment}>
-        {" "}
-        Start your collection <br /> Buy now
-      </Button>
-      {/* <div> console.log(The NFT buying intention is { count })</div> */}
-    </>
+    <Card style={{ background: "linear-gradient(#B2FBED, #9198e5)" }} className="w-100, h-100 " >
+      <Card.Body className="d-flex flex-wrap align-items-center align-content-center justify-content-center" >
+        <Card.Text  className="d-grip  p-5 fs-3 fw-bold " > Start your Collection</Card.Text>
+          <Button gb="primary" onClick={increment} size="lg" className="d-grip  m-5 fs-3 ">
+            Buy now
+          </Button>
+        
+      </Card.Body>
+    </Card>
   );
 };
 
