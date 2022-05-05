cb8e7e50e6bfdb0201476cdd8bd250cc3cde4c0d
PaolaP <paolaperez8@gmail.com>
2022-05-04 23:09:25 -0400

    chore: fix img path

--- frontend/src/views/NftGallery.jsx
+++ frontend/src/views/NftGallery.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import { Carousel } from "react-bootstrap";
+import { Carousel, Button } from "react-bootstrap";
 import Navigation from "../components/Navigation";
-import Buy from "../components/Buy";
+
 
 function NftGallery() {
   return (
@@ -10,45 +10,53 @@ function NftGallery() {
       <Navigation />
       <Carousel  className="m-3 p-4">
         <Carousel.Item  >
-        <img className="d-block w-100" src="athens.png" alt="Third slide" />
+        <img className="d-block w-100" src="images/athens.png" alt="Third slide" />
 
           <Carousel.Caption>
             <h3> Athens 2022</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
-            <Buy />
+            <Button className= "fw-bold" style={{ background: "linear-gradient(#B2FBED, #9198e5)" }}>
+              + Details
+            </Button>
           </Carousel.Caption>
         </Carousel.Item >
         <Carousel.Item >
-          <img className="d-block w-100" src="berlin.png" alt="Second slide" />
+          <img className="d-block w-100" src="images/berlin.png" alt="Second slide" />
 
           <Carousel.Caption>
             <h3> Berlin 2022</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <Buy />
+            <Button className= "fw-bold" style={{ background: "linear-gradient(#B2FBED, #9198e5)" }}>
+              + Details
+            </Button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item >
-          <img className="d-block w-100" src="cairo.png" alt="Third slide" />
+          <img className="d-block w-100" src="images/cairo.png" alt="Third slide" />
 
           <Carousel.Caption>
             <h3>Cairo 2022</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
-            <Buy />
+            <Button className= "fw-bold" style={{ background: "linear-gradient(#B2FBED, #9198e5)" }}>
+              + Details
+            </Button>
           </Carousel.Caption>
         </Carousel.Item >
         <Carousel.Item >
-          <img className="d-block w-100" src="lisbon.png" alt="Third slide" />
+          <img className="d-block w-100" src="images/lisbon.png" alt="Third slide" />
 
           <Carousel.Caption>
             <h3> Lisbon 2022</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
-            <Buy />
+            <Button className= "fw-bold" style={{ background: "linear-gradient(#B2FBED, #9198e5)" }}>
+              + Details
+            </Button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
