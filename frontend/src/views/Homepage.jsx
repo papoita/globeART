a78d389dc79c3a1ff8f95cefc82bd342fb39e529
PaolaP <paolaperez8@gmail.com>
2022-05-06 22:12:47 -0400

    feature: sizing using SizeMe pck

--- frontend/src/views/Homepage.jsx
+++ frontend/src/views/Homepage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "../App.css";
 import { Row, Col } from "react-bootstrap";
 //import ReactGlobe from 'react-globe.gl';
+import { SizeMe } from 'react-sizeme'
 
 //import SceneView from "../components/SceneView";
 import Navigation from "../components/Navigation";
@@ -11,17 +12,18 @@ import Footer from "../components/Footer";
 import Location from "../components/Location";
 import SimpleGlobe from "../components/globe";
 
-
 function Homepage() {
   return (
-    
-    
     <div className="Homepage">
       <Navigation />
-      <Location/>
+      <Location />
       <Row>
-        <Col  sm={9} style={{ paddingRight: "0px" }}>
-          <SimpleGlobe height="10vh" width={10}/>
+        <Col sm={9} style={{ paddingRight: "0px" }}>
+          <SizeMe>
+            {({ size: { width } }) => (
+              <SimpleGlobe width={width} height={width / (4 / 3)} />
+            )}
+          </SizeMe>
         </Col>
 
         <Col sm={3} style={{ paddingLeft: "0px" }}>
@@ -30,7 +32,6 @@ function Homepage() {
       </Row>
       <Footer />
     </div>
-    
   );
 }
 
