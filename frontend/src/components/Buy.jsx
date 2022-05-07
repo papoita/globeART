7621ff685f61a0e13032792a8d21bc6edab03d0b
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-05-06 23:09:02 -0400

    feature: add connect and disconnect button to Buy page

--- frontend/src/components/Buy.jsx
+++ frontend/src/components/Buy.jsx
@@ -1,34 +1,47 @@
 import React from "react";
-import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
-
-// let count = 0;
+import useMetaMask from "../hooks/useMetamask";
 
 const Buy = () => {
-  // const arr = useState(0);
-  // const count = arr[0];
-  // const setCount = arr[1];
-
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-    console.log(count);
-  };
+  const { connect, isActive, account, disconnect, isDisable } = useMetaMask();
 
   return (
-    <Card style={{ background: "linear-gradient(#B2FBED, #9198e5)" }}  className="d-flex flex-wrap text-center">
-      <Card.Body  >
-        <Card.Text  className="d-grip  p-1 fs-3 fw-bold align-content-center" > Start your Collection</Card.Text>
-        <Card.Text  className="d-grip  p-1 fs-5  align-content-center" > The 2022 Collection is very special as the first one that globeART has created, expect amazing graphics and contrasting colors to match that adrenaline rush of traveling the world again! </Card.Text>
-          <Button gb="primary" onClick={increment} size="lg" className="d-grip m-2 p-3  fs-4 align-middle text-center">
-            Buy now
-          </Button>
-        
+    <Card
+      style={{ background: "linear-gradient(#B2FBED, #9198e5)" }}
+      className="d-flex flex-wrap text-center">
+      <Card.Body>
+        <Card.Text className="d-grip  p-1 fs-3 fw-bold align-content-center">
+          {" "}
+          Start your Collection
+        </Card.Text>
+        <Card.Text className="d-grip  p-1 fs-5  align-content-center">
+          {" "}
+          The 2022 Collection is very special as the first one that globeART has
+          created, expect amazing graphics and contrasting colors to match that
+          adrenaline rush of traveling the world again!{" "}
+        </Card.Text>
+        <Button
+          gb="primary"
+          onClick={connect}
+          disabled={isDisable}
+          size="md"
+          className="d-grip m-2 p-3  fs-4 align-middle text-center">
+          Connect to MetaMask
+        </Button>
+        {/* TODO: For visibility only, to be removed later */}
+        <div className="mt-2 mb-2">
+          Connected Account: {isActive ? account : ""}
+        </div>
+        <Button
+          gb="primary"
+          onClick={disconnect}
+          size="md"
+          className="d-grip m-2 p-3  fs-4 align-middle text-center">
+          Disconnect MetaMask
+        </Button>
       </Card.Body>
     </Card>
   );
 };
 
 export default Buy;
-
