2875ccd06a859644e9662f036255a259eafb64e8
PaolaP <paolaperez8@gmail.com>
2022-05-06 21:45:17 -0400

    feature: replace esri globe with 3js globe that has labels

--- frontend/src/views/Homepage.jsx
+++ frontend/src/views/Homepage.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import "../App.css";
 import { Row, Col } from "react-bootstrap";
+//import ReactGlobe from 'react-globe.gl';
 
-import SceneView from "../components/SceneView";
+//import SceneView from "../components/SceneView";
 import Navigation from "../components/Navigation";
 import Aside from "../components/Aside";
 import Footer from "../components/Footer";
 import Location from "../components/Location";
+import SimpleGlobe from "../components/globe";
 
 
 function Homepage() {
@@ -18,8 +20,8 @@ function Homepage() {
       <Navigation />
       <Location/>
       <Row>
-        <Col sm={9} style={{ paddingRight: "0px" }}>
-          <SceneView />
+        <Col  sm={9} style={{ paddingRight: "0px" }}>
+          <SimpleGlobe height="10vh" width={10}/>
         </Col>
 
         <Col sm={3} style={{ paddingLeft: "0px" }}>
