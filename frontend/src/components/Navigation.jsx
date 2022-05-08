1a1d8714e29947f3f4db9f3dc9b4c3ba1068c48c
Martha Staus <mjstaus@gmail.com>
2022-05-08 06:58:41 -0700

    FEATURE: Add connect to Metamask button

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Container, NavDropdown, Image } from "react-bootstrap";
+import { Navbar, Nav, Container, Button, Image } from "react-bootstrap";
+
+import useMetaMask from "../hooks/useMetamask";
 
 function Navigation() {
+  const { connect, isActive, account, disconnect, isDisable } = useMetaMask();
+
   return (
     <Navbar className="sticky-top" bg="primary" variant="dark" expand="lg">
       <Container>
@@ -23,23 +27,29 @@ function Navigation() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link className="fw-bold" as={Link} to="/nftglobegallery">
-              NFT Art Gallery
+              Gallery
             </Nav.Link>
-            <Nav.Link href="#link">About Us</Nav.Link>
-
-            <NavDropdown title="Collections" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/nftglobegallery">
-                Gallery globeArt
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/nftbuyitem">
-                Buy globeART
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/personalcollection">
-                My Collection
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link"> About Us </Nav.Link>
+            
+            <Nav.Link className="fw-bold" as={Link} to="/personalcollection">
+              My Collection
+            </Nav.Link>
+            <Nav.Link className="fw-bold" as={Link} to="/nftbuyitem">
+              Available Now
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
+      </Container>
+
+      <Container>
+        <Button
+            variant="secondary"
+            onClick={connect}
+            disabled={isDisable}
+            size="sm"
+            className="m-2 p-2 fs-6 align-middle text-center">
+            Connect MetaMask
+          </Button>
       </Container>
     </Navbar>
   );
