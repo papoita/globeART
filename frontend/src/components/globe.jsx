399054fd770d1c630d40d2e8261eebd6cfb6260c
Martha Staus <mjstaus@gmail.com>
2022-05-10 07:42:39 -0700

    FEATURE: Add onLabelClick event - open modal

--- frontend/src/components/globe.jsx
+++ frontend/src/components/globe.jsx
@@ -1,36 +1,56 @@
-import React from 'react';
+import {useState} from 'react';
 import ReactGlobe from 'react-globe.gl';
 import places from './places'; 
-
-
+import { Modal, Button } from 'react-bootstrap';
 
   const SimpleGlobe = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
     const props = {
-      
       setFocus: {
         "New York": [40.73061, -73.935242]
-      }
+      },
     };
-   
-   
-    return <ReactGlobe {...props}
+
+    return (
+    <>
+    <ReactGlobe {...props}
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
       backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-     
       
-
       labelsData={places}
-      labelLat={d => d.lat}
-      labelLng={d => d.lng}
-      labelText={d => d.name}
-      labelSize={d => 0.5 + d.size }
-      labelDotRadius={d => 0.5 + d.size}
-      labelColor={() => 'rgba(238, 130, 238, 0.75)'}
-      labelResolution={2}
-      // labelLink={d => 'href=d.link'}
+        labelLat={d => d.lat}
+        labelLng={d => d.lng}
+        labelText={d => d.name}
+        labelSize={d => 0.5 + d.size }
+        labelDotRadius={d => 0.5 + d.size}
+        labelColor={d => d.color}
+        labelResolution={2}
+        onLabelClick={() => handleShow()}
+        // labelLink={d =>`href=${d.link}`}
     />;
+    <Modal show={show} onHide={() => handleClose()}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => handleClose()}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+    )
   };
 
   export default SimpleGlobe;
 
   //onGlobeClick?: (coords: { lat, lng }, event: MouseEvent) => void;
+
+  // {d => d.addEventListener('click', (event) => {
+  //   event.stopPropagation();
+  //   return console.log(`node clicked: #${d.name}`);
+  // })}
