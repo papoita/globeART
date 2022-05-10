0bf55afaa29760f05d98eec40cf6d39e1e02596f
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-05-10 15:54:28 -0400

    feature: render connect/disconnect button in homepage

--- frontend/src/components/Aside.jsx
+++ frontend/src/components/Aside.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
-const Aside = () => {
+const Aside = ({ account, connect, disconnect, isActive }) => {
   return (
     <Card className="rounded-0 " border="primary" style={{ height: 926 }}>
       <Card.Img className="rounded-0" variant="top" src="./samplenft.png" />
       <Card.Body variant="dark">
-        <Card.Img  src="./logonamesm.png"></Card.Img>
-        <Card.Title className="fs-4 fw-bold">NFT art collection for wanderlusts</Card.Title>
+        <Card.Img src="./logonamesm.png"></Card.Img>
+        <Card.Title className="fs-4 fw-bold">
+          NFT art collection for wanderlusts
+        </Card.Title>
         <Card.Text className="p-1">
           Do you love travelling and art, but tired of collecting fridge
           magnets?
@@ -18,11 +19,19 @@ const Aside = () => {
           will remind you of the places that you have been and the memories that
           you have.
         </Card.Text>
-        <Card.Text className="p-1">The more you travel, the more you can collect.</Card.Text>
-
-        <Button className="m-1 p-1  fs-5 fw-bold " style={{ background: "linear-gradient(#B2FBED, #9198e5)" }} as={Link} to="/gallery">
-          Start your Collection
+        <Card.Text className="p-1">
+          The more you travel, the more you can collect.
+        </Card.Text>
+        <Button
+          gb="primary"
+          size="md"
+          className="d-grip m-2 p-3  fs-4 align-middle text-center"
+          onClick={isActive ? disconnect : connect}>
+          {isActive ? "Disconnect MetaMask" : "Connect to MetaMask"}
         </Button>
+        <div className="mt-2 mb-2">
+          Connected Account: {isActive ? account : ""}
+        </div>
       </Card.Body>
     </Card>
   );
