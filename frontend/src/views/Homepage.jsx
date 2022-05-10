783d1d3df64e4ad138540405b9cd21eb7c59e4fa
Martha Staus <mjstaus@gmail.com>
2022-05-10 08:29:56 -0700

    FEATURE: Add show, nft states. Add handlers for showing modal. Add NftModal component pass handleShow function as prop to SimpleGlobe component

--- frontend/src/views/Homepage.jsx
+++ frontend/src/views/Homepage.jsx
@@ -1,21 +1,33 @@
 import "../App.css";
+import {useState} from 'react';
 import { Row, Col } from "react-bootstrap";
 import { SizeMe } from "react-sizeme";
 import Navigation from "../components/Navigation";
 import Aside from "../components/Aside";
 import Footer from "../components/Footer";
 import SimpleGlobe from "../components/globe";
+import NftModal from "../components/NftModal";
 
 function Homepage({ account, web3Handler, location }) {
+  const [show, setShow] = useState(false);
+  const [nft, setNft] = useState({})
+    
+  const handleClose = () => setShow(false);
+  const handleShow = (d) => {
+    setShow(true);
+    setNft(d)
+  }
+
   console.log(location);
   return (
     <div className="Homepage">
       <Navigation account={account} web3Handler={web3Handler} />
+      <NftModal nft={nft} show={show} onHide={() => handleClose()}/>
       <Row>
         <Col sm={9} style={{ paddingRight: "0px" }}>
           <SizeMe>
             {({ size: { width } }) => (
-              <SimpleGlobe width={width} height={width / (4 / 3)} />
+              <SimpleGlobe width={width} height={width / (4 / 3)} handleShow={handleShow} />
             )}
           </SizeMe>
         </Col>
