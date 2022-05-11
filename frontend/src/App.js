5682bbbf02e9db2e6542906d982e38be63f4ad4d
Martha Staus <mjstaus@gmail.com>
2022-05-10 20:27:03 -0700

    FEATURE: Add purchases variable from useWeb3, remove unused variables and props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Homepage from "./views/Homepage";
 import Gallery from "./views/Gallery";
-import NFT from "./views/NFT";
 import PersonalCollection from "./views/PersonalCollection";
 import AvailableNFT from "./views/AvailableNFT";
 import SimpleGlobe from "./components/globe";
@@ -19,22 +18,17 @@ import useGeolocation from "./hooks/useGeolocation";
 function App() {
   const {
     items,
-    store,
-    account,
     isLoading,
-    nft,
     web3Handler,
-    loadStoreItems,
     buyStoreItem,
+    purchases,
+    account
   } = useWeb3();
   const { location } = useGeolocation();
 
   useEffect(() => {
     web3Handler();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  console.log("App variable", store);
 
   return (
     <Router>
@@ -46,9 +40,6 @@ function App() {
             <Gallery
               account={account}
               web3Handler={web3Handler}
-              nft={nft}
-              store={store}
-              loadStoreItems={loadStoreItems}
               items={items}
               loading={isLoading}
               buyStoreItem={buyStoreItem}
@@ -76,10 +67,9 @@ function App() {
           path="/personalcollection"
           element={
             <PersonalCollection
-              account={account}
               web3Handler={web3Handler}
-              nft={nft}
-              store={store}
+              account={account}
+              purchases={purchases}
             />
           }
         />
