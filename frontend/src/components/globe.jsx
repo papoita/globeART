e8b95fab23c90365addf62f7073d6c71107ad921
PaolaP <paolaperez8@gmail.com>
2022-05-11 13:07:06 -0400

    add link to gallery

--- frontend/src/components/globe.jsx
+++ frontend/src/components/globe.jsx
@@ -1,47 +1,61 @@
-import {useEffect, useRef} from 'react';
-import ReactGlobe from 'react-globe.gl';
-import places from './places'; 
+import { useEffect, useRef } from "react";
+import ReactGlobe from "react-globe.gl";
+import places from "./places";
+import { useNavigate } from "react-router-dom";
 
-
-
-  const SimpleGlobe = () => {
-   
-    const globeEl = useRef()
-    useEffect(() => {
-      globeEl.current.controls().autoRotate = true;
-      globeEl.current.controls().autoRotateSpeed = 0.8;
+const SimpleGlobe = () => {
+  const globeEl = useRef();
+  const navigate = useNavigate();
   
-      const MAP_CENTER = { lat: 45, lng: -75, altitude: 1.7 };
-      globeEl.current.pointOfView(MAP_CENTER, 0);
-    }, [globeEl]);
-   
-   
-    return <ReactGlobe 
-    
-    ref={globeEl}
-    showAtmosphere={true}
+  useEffect(() => {
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.8;
+
+    const MAP_CENTER = { lat: 45, lng: -75, altitude: 1.7 };
+    globeEl.current.pointOfView(MAP_CENTER, 0);
+  }, [globeEl]);
+
+  
+
+  return (
+    <ReactGlobe
+
+      ref={globeEl}
+      showAtmosphere={true}
       atmosphereAltitude={0.2}
-      atmosphereColor= "purple"
+      atmosphereColor="purple"
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
       backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-    
-      
-
       labelsData={places}
-      labelLat={d => d.lat}
-      labelLng={d => d.lng}
-      labelText={d => d.name}
-      labelSize={d => 0.8 + d.size }
-      labelDotRadius={d => 0.8 + d.size}
-      labelColor={() => 'rgba(237,0,248, 0.95)'}
+      labelLat={(d) => d.lat}
+      labelLng={(d) => d.lng}
+      labelText={(d) => d.name}
+      labelSize={(d) => 0.8 + d.size}
+      labelDotRadius={(d) => 0.8 + d.size}
+      labelColor={() => "rgba(237,0,248, 0.95)"}
       labelResolution={2}
+      onGlobeClick={()=> navigate("/gallery")}
       // labelLink={d => 'href=d.link'}
-    />;
-  };
+      
+    />
+  );
+};
 
-  export default SimpleGlobe;
+export default SimpleGlobe;
 
-  //onGlobeClick?: (coords: { lat, lng }, event: MouseEvent) => void;
+//onGlobeClick?: (coords: { lat, lng }, event: MouseEvent) => void;
 
-  //onGlobeClick({ lat, lng }, event)
- // will take you to the availvable item:id
+//onGlobeClick({ lat, lng }, event)
+// will take you to the availvable item:id
+{/* <button
+        onClick={() => {
+          // overlap
+          //globeEl.current.pointOfView({ lat: 30, lng: 7, altitude: 2 }, 5000)
+          globeEl.current.pointOfView(
+            { lat: 60, lng: 180, altitude: 1 },
+            15000
+          );
+        }}
+      >
+        test
+      </button> */}
