0949a79bd42a6cbf01b3ddac6e717faaa1b90ab3
PaolaP <paolaperez8@gmail.com>
2022-05-12 01:19:16 -0400

    improve style footer and clean up

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+
 import { Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-import { FaBeer } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
 
 function Footer() {
   return (
-    <Card className="text-white text-center" bg="primary" variant="light">
-      <Card.Header>Contact Us</Card.Header>
+    <Card
+      className="text-white text-center text-muted"
+      bg="primary"
+      variant="light"
+    >
+      <Card.Header>Contact Us </Card.Header>
 
-      <Card.Footer className="text-white">
+      <Card.Footer
+        justify-content="space-around"
+        className="text-white text-muted "
+      >
         <FaGithub />
-        <FaTwitter />
         Want to chat about awesome ideas? let's get some coffee
         <FontAwesomeIcon
           icon={faCoffee}
