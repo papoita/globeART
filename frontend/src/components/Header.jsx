e6f9059f70506eb680474f58e7f40788fffc05a7
Martha Staus <mjstaus@gmail.com>
2022-05-12 06:43:55 -0700

    REFACTOR: Comment out components relying on bootstrap - prepare for  Bootstrap uninstall

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,24 @@
-import React from "react";
+// import React from "react";
 
-const Header = (props) => {
-  console.log("props", props);
+// const Header = (props) => {
+//   console.log("props", props);
 
-  const projectDescription = (
-    <p>
-      Do you love travelling and art, but tired of collecting fridge magnets?
-      This beautiful NFT artwork only obtainable when you visit the city, will
-      remind you of the places that you have been and the memories that you
-      have. The more you travel, the more you can collect.
-    </p>
-  );
+//   const projectDescription = (
+//     <p>
+//       Do you love travelling and art, but tired of collecting fridge magnets?
+//       This beautiful NFT artwork only obtainable when you visit the city, will
+//       remind you of the places that you have been and the memories that you
+//       have. The more you travel, the more you can collect.
+//     </p>
+//   );
 
-  return (
-    <>
-      <h2>{props.message} </h2>
-      <h3> Hello {props.name} </h3>
-      {projectDescription}
-    </>
-  );
-};
+//   return (
+//     <>
+//       <h2>{props.message} </h2>
+//       <h3> Hello {props.name} </h3>
+//       {projectDescription}
+//     </>
+//   );
+// };
 
-export default Header;
+// export default Header;
