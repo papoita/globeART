18794d254a76de668c905ef35bb70a13955377b9
PaolaP <paolaperez8@gmail.com>
2022-05-12 19:54:27 -0400

    fix aside size

--- frontend/src/views/Homepage.jsx
+++ frontend/src/views/Homepage.jsx
@@ -19,7 +19,7 @@ function Homepage({
     <div className="Homepage">
       <Navigation account={account} web3Handler={web3Handler} />
       <Row>
-        <Col sm={9} style={{ paddingRight: "0px" }}>
+        <Col sm={10} style={{ paddingRight: "0px" }}>
           <SizeMe>
             {({ size: { width } }) => (
               <SimpleGlobe width={width} height={width / (4 / 3)} />
@@ -27,8 +27,9 @@ function Homepage({
           </SizeMe>
         </Col>
 
-        <Col sm={3} style={{ paddingLeft: "0px" }}>
+        <Col sm={2} className="d-flex align-items-center justify-content-center " style={{ paddingLeft: "0px" }}>
           <Aside
+          
             account={account}
             connect={connect}
             disconnect={disconnect}
