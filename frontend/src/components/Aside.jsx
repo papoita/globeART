ecbcede18d5bce841dbc8dbb55f279a276cd82d5
PaolaP <paolaperez8@gmail.com>
2022-05-12 19:50:53 -0400

    add icon wallet, fix button s izing add descriptor for connector

--- frontend/src/components/Aside.jsx
+++ frontend/src/components/Aside.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import { FaWallet } from "react-icons/fa";
 
 const Aside = ({ account, connect, disconnect, isActive }) => {
   return (
-    <Card className="rounded-0 " border="primary" style={{ height: 926 }}>
+    <Card className="rounded-0 " border="primary" style={{ height: 800 }}>
       <Card.Img className="rounded-0" variant="top" src="./samplenft.png" />
       <Card.Body variant="dark">
         <Card.Img src="./logonamesm.png"></Card.Img>
@@ -11,22 +12,25 @@ const Aside = ({ account, connect, disconnect, isActive }) => {
           NFT art collection for wanderlusts
         </Card.Title>
         <Card.Text className="p-1">
-          Do you love travelling and art, but tired of collecting fridge
+          Do you love travelling and art, but are tired of collecting fridge
           magnets?
         </Card.Text>
         <Card.Text className="p-1">
-          This beautiful NFT artwork only obtainable when you visit the city,
+          This beautiful NFT artwork is only obtainable when you visit the city. It 
           will remind you of the places that you have been and the memories that
           you have.
         </Card.Text>
         <Card.Text className="p-1">
           The more you travel, the more you can collect.
         </Card.Text>
-
+        <Card.Text className="p-1 fw-bold">
+          Browse the 2022 collection by connecting to your wallet 
+        </Card.Text>
+        <FaWallet  style={{color:"blue", size:4}}/>
         <Button
           gb="primary"
-          size="md"
-          className="d-grip m-2 p-3  fs-4 align-middle text-center"
+          
+          className="d-grip m-2 p-1  fs-5 align-middle text-center"
           onClick={isActive ? disconnect : connect}>
           {isActive ? "Disconnect MetaMask" : "Connect to MetaMask"}
         </Button>
