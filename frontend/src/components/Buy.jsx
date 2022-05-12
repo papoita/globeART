c6d3838d2dae8299852b6e83b00845400a41ff82
PaolaP <paolaperez8@gmail.com>
2022-05-12 01:38:03 -0400

    styling buy component

--- frontend/src/components/Buy.jsx
+++ frontend/src/components/Buy.jsx
@@ -7,15 +7,15 @@ const Buy = ({ buyStoreItem, item }) => {
       style={{ background: "linear-gradient(#B2FBED, #9198e5)" }}
       className="d-flex flex-wrap text-center">
       <Card.Body>
-        <Card.Text className="d-grip  p-1 fs-3 fw-bold align-content-center">
-          {" "}
+        <Card.Text className="d-grip  p-1 fs-1 fw-bold align-content-center">
+         
           Start your Collection
         </Card.Text>
-        <Card.Text className="d-grip  p-1 fs-5  align-content-center">
-          {" "}
+        <Card.Text className="d-grip  p-4 fs-5  align-content-center">
+          
           The 2022 Collection is very special as the first one that globeART has
           created, expect amazing graphics and contrasting colors to match that
-          adrenaline rush of traveling the world again!{" "}
+          adrenaline rush of traveling the world again!
         </Card.Text>
         <Button
           gb="primary"
