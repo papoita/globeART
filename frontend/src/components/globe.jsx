fafae91b96568a18cc7cd597859fb7e38d861af5
Martha Staus <mjstaus@gmail.com>
2022-05-12 06:35:50 -0700

    SETUP: Insert globe component logic from globe-interactive-map branch

--- frontend/src/components/globe.jsx
+++ frontend/src/components/globe.jsx
@@ -1,40 +1,66 @@
-import {useEffect, useRef} from 'react';
+import React from 'react';
 import ReactGlobe from 'react-globe.gl';
 import places from './places'; 
 
   const Globe = () => {
-   
-    const globeEl = useRef()
-    // useEffect(() => {
-    //   globeEl.current.controls().autoRotate = true;
-    //   globeEl.current.controls().autoRotateSpeed = 0.8;
-  
-    //   const MAP_CENTER = { lat: 45, lng: -75, altitude: 1.7 };
-    //   globeEl.current.pointOfView(MAP_CENTER, 0);
-    // }, [globeEl]);
-   
-   
-    return <ReactGlobe 
-    
-    ref={globeEl}
-    showAtmosphere={true}
-      atmosphereAltitude={0.2}
-      atmosphereColor= "purple"
+
+    const props = {
+      setFocus: {
+        "New York": [40.73061, -73.935242]
+      },
+    };
+
+    return (
+    <>
+    <ReactGlobe {...props}
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
       backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      
+      labelsData={places}
+        labelLat={d => d.lat}
+        labelLng={d => d.lng}
+        labelText={d => d.name}
+        labelSize={d => 0.5 + d.size }
+        labelDotRadius={d => 0.5 + d.size}
+        labelColor={d => d.color}
+        labelResolution={2}
+        // onLabelClick={(d) => handleShow(d)}
+        // labelLink={d =>`href=${d.link}`}
+    />;
+    </>
+    )
+   
+  //   const globeEl = useRef()
+  //   // useEffect(() => {
+  //   //   globeEl.current.controls().autoRotate = true;
+  //   //   globeEl.current.controls().autoRotateSpeed = 0.8;
+  
+  //   //   const MAP_CENTER = { lat: 45, lng: -75, altitude: 1.7 };
+  //   //   globeEl.current.pointOfView(MAP_CENTER, 0);
+  //   // }, [globeEl]);
+   
+   
+  //   return <ReactGlobe 
+    
+  //   ref={globeEl}
+  //   showAtmosphere={true}
+  //     atmosphereAltitude={0.2}
+  //     atmosphereColor= "purple"
+  //     globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+  //     backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
     
       
 
-      labelsData={places}
-      labelLat={d => d.lat}
-      labelLng={d => d.lng}
-      labelText={d => d.name}
-      labelSize={d => 0.8 + d.size }
-      labelDotRadius={d => 0.8 + d.size}
-      labelColor={() => 'rgba(237,0,248, 0.95)'}
-      labelResolution={2}
-      // labelLink={d => 'href=d.link'}
-    />;
+  //     labelsData={places}
+  //     labelLat={d => d.lat}
+  //     labelLng={d => d.lng}
+  //     labelText={d => d.name}
+  //     labelSize={d => 0.8 + d.size }
+  //     labelDotRadius={d => 0.8 + d.size}
+  //     labelColor={() => 'rgba(237,0,248, 0.95)'}
+  //     labelResolution={2}
+  //     // labelLink={d => 'href=d.link'}
+  //   />;
   };
 
   export default Globe;
