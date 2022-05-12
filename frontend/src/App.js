4076a4bc42b2eb323a445137599c927420498ff4
Martha Staus <mjstaus@gmail.com>
2022-05-12 06:42:38 -0700

    REFACTOR: Comment out components relying on bootstrap - prepare for  Bootstrap uninstall

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,7 @@ import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Homepage from "./views/Homepage";
-import Gallery from "./views/Gallery";
-import PersonalCollection from "./views/PersonalCollection";
-import AvailableNFT from "./views/AvailableNFT";
-import Globe from "./components/Globe";
+import Home from "./views/Home";
 import useWeb3 from "./hooks/useWeb3";
 import useGeolocation from "./hooks/useGeolocation";
 import useMetaMask from "./hooks/useMetamask";
@@ -27,7 +23,7 @@ function App() {
         <Route
           path="/"
           element={
-            <Homepage
+            <Home
               location={location}
               connect={connect}
               disconnect={disconnect}
@@ -37,41 +33,6 @@ function App() {
             />
           }
         />
-        <Route
-          path="/gallery"
-          element={
-            <Gallery
-              account={account}
-              web3Handler={web3Handler}
-              items={items}
-              loading={isLoading}
-              buyStoreItem={buyStoreItem}
-            />
-          }
-        />
-        <Route
-          path="/personalcollection"
-          element={
-            <PersonalCollection
-              web3Handler={web3Handler}
-              account={account}
-              purchases={purchases}
-            />
-          }
-        />
-        <Route
-          path="/nft"
-          element={
-            <AvailableNFT
-              location={location}
-              items={items}
-              account={account}
-              web3Handler={web3Handler}
-              buyStoreItem={buyStoreItem}
-            />
-          }
-        />
-        <Route path="/globe" element={< Globe />} />
       </Routes>
     </Router>
   );
