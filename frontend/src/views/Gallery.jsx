129d711d58d7ff0cfa68c7782181741c0d7ed18b
PaolaP <paolaperez8@gmail.com>
2022-05-12 00:07:43 -0400

    fade animation for card

--- frontend/src/views/Gallery.jsx
+++ frontend/src/views/Gallery.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import { useState } from "react";
 import {
   Carousel,
   Button,
+  Fade,
   Card,
   CardGroup,
   Container,
@@ -19,6 +20,8 @@ function Gallery({
   account,
   loading,
 }) {
+
+  const [open, setOpen] = useState(false);
 
   console.log("ITEMS", items);
 
@@ -52,7 +55,50 @@ function Gallery({
             ))}
         </Carousel>
       </Container>
-
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-fade-text"
+        aria-expanded={open}
+      >
+        Toggle text
+      </Button>
+      <Fade in={open}>
+        <div id="example-fade-text">
+        <CardGroup className="m-4">
+        <Container>
+          <Row>
+            <h2>GlobeART Collections</h2>
+            {items.length > 0 ? (
+              items.map((item, idx) => (
+                <Card key={idx} className="m-4">
+                  <Card.Img
+                    variant="top"
+                    src={item.image}
+                    style={{ width: "400px" }}
+                  />
+                  <Card.Body
+                    style={{
+                      background: "linear-gradient(#B2FBED, #9198e5)",
+                      width: "400px",
+                    }}>
+                    <Card.Title>{item.collection}</Card.Title>
+                    <Card.Text>
+                      <small bg="primary">Price: {item.price} ETH</small>
+                    </Card.Text>
+                    <Button onClick={() => buyStoreItem(item)}>Buy Now!</Button>
+                  </Card.Body>
+                </Card>
+              ))
+            ) : (
+              <div style={{ padding: "1rem 0" }}>
+                <h2>No listed assets</h2>
+              </div>
+            )}
+          </Row>
+        </Container>
+      </CardGroup>
+        </div>
+      </Fade>
      
       <Footer />
     </>
