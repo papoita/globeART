eb93247a9a5256eeb8c3c0e7a3a4d4a2b1a733ca
PaolaP <paolaperez8@gmail.com>
2022-05-12 20:42:28 -0400

    btn fix

--- frontend/src/components/Aside.jsx
+++ frontend/src/components/Aside.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 import { FaWallet } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const Aside = ({ account, connect, disconnect, isActive }) => {
   return (
     <Card className="rounded-0 " border="primary" style={{ height: 800 }}>
       <Card.Img className="rounded-0" variant="top" src="./samplenft.png" />
-      <Card.Body variant="dark">
+      <Card.Body variant="dark"  >
         <Card.Img src="./logonamesm.png"></Card.Img>
         <Card.Title className="fs-4 fw-bold">
           NFT art collection for wanderlusts
@@ -16,30 +17,29 @@ const Aside = ({ account, connect, disconnect, isActive }) => {
           magnets?
         </Card.Text>
         <Card.Text className="p-1">
-          This beautiful NFT artwork is only obtainable when you visit the city. It 
-          will remind you of the places that you have been and the memories that
-          you have.
+          This beautiful NFT artwork is only obtainable when you visit the city.
+          It will remind you of the places that you have been and the memories
+          that you have.
         </Card.Text>
         <Card.Text className="p-1">
           The more you travel, the more you can collect.
         </Card.Text>
         <Card.Text className="p-1 fw-bold">
-          Browse the 2022 collection by connecting to your wallet 
+          Browse the 2022 collection by connecting to your{" "}
+          <IconContext.Provider value={{ color: "blue", size: 20 }}>
+            <FaWallet />
+          </IconContext.Provider>
         </Card.Text>
-        <FaWallet  style={{color:"blue", size:4}}/>
-        <Button
-          gb="primary"
-          
-          className="d-grip m-2 p-1  fs-5 align-middle text-center"
-          onClick={isActive ? disconnect : connect}>
-          {isActive ? "Disconnect MetaMask" : "Connect to MetaMask"}
-        </Button>
-        <div className="mt-2 mb-2">
+        <Button variant="light" className="mt-2 d-grid gap-2 mb-2 text-center align-middle"  style={{ background: "linear-gradient(#B2FBED, #9198e5)" }} onClick={isActive ? disconnect : connect}>{isActive ? "Disconnect MetaMask" : "Connect to MetaMask"}</Button>
+
+        
+        {/* <div className="mt-2 mb-2">
           Connected Account: {isActive ? account : ""}
-        </div>
+        </div> */}
       </Card.Body>
     </Card>
   );
 };
 
 export default Aside;
+//
