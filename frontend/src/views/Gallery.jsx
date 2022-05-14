d5823f54a17b05a807a2cf9afb5516dc6edd7109
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-05-13 22:24:07 -0400

    fix: remove the Buy Now button from the Gallery view

--- frontend/src/views/Gallery.jsx
+++ frontend/src/views/Gallery.jsx
@@ -13,15 +13,7 @@ import {
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 
-
-function Gallery({
-  web3Handler,
-  items,
-  buyStoreItem,
-  account,
-  loading,
-}) {
-
+function Gallery({ web3Handler, items, account, loading }) {
   const [open, setOpen] = useState(false);
 
   console.log("ITEMS", items);
@@ -37,14 +29,13 @@ function Gallery({
             items.map((item, idx) => (
               <Carousel.Item key={idx}>
                 <img
-                style={{height:650}}
+                  style={{ height: 650 }}
                   className="rounded mx-auto d-block"
                   src={item.image}
                   alt={item.name}
                 />
                 <Carousel.Caption>
                   <h3>{item.name} 2022</h3>
-                 
                 </Carousel.Caption>
               </Carousel.Item>
             ))}
@@ -55,51 +46,43 @@ function Gallery({
         aria-controls="collapse-text"
         aria-expanded={open}
         className="fw-bold m-1 d-grid gap-2 col-3 mx-auto"
-        style={{ background: "linear-gradient(#B2FBED, #9198e5)" }} size="md" variant="light">
-      
+        style={{ background: "linear-gradient(#B2FBED, #9198e5)" }}
+        size="md"
+        variant="light">
         View All
       </Button>
       <Collapse in={open}>
         <div id="collapse-text">
-  
-        <CardGroup className=" mx-auto " >
-        <Container>
-          <Row>
-          
-            <Row xs={5}  className="g-4">
-            {items.length > 0 ? (
-              items.map((item, idx) => (
-                <Card key={idx} className="m-4 text-center">
-                  <Card.Img
-                    variant="top"
-                    src={item.image}
-                    
-                  />
-                  <Card.Body
-                    style={{
-                      background: "linear-gradient(#B2FBED, #9198e5)",
-                      
-                    }}>
-                    <Card.Title>{item.collection}</Card.Title>
-                    <Card.Text>
-                      <small bg="primary">Price: {item.price} ETH</small>
-                    </Card.Text>
-                    <Button onClick={() => buyStoreItem(item)}>Buy Now!</Button>
-                  </Card.Body>
-                </Card>
-              ))
-            ) : (
-              <div style={{ padding: "1rem 0" }}>
-                <h2>No listed assets</h2>
-              </div>
-            )}
-          </Row>
-          </Row>
-        </Container>
-      </CardGroup>
+          <CardGroup className=" mx-auto ">
+            <Container>
+              <Row>
+                <Row xs={5} className="g-4">
+                  {items.length > 0 ? (
+                    items.map((item, idx) => (
+                      <Card key={idx} className="m-4 text-center">
+                        <Card.Img variant="top" src={item.image} />
+                        <Card.Body
+                          style={{
+                            background: "linear-gradient(#B2FBED, #9198e5)",
+                          }}>
+                          <Card.Title>{item.collection}</Card.Title>
+                          <Card.Text>
+                            <small bg="primary">Price: {item.price} ETH</small>
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    ))
+                  ) : (
+                    <div style={{ padding: "1rem 0" }}>
+                      <h2>No listed assets</h2>
+                    </div>
+                  )}
+                </Row>
+              </Row>
+            </Container>
+          </CardGroup>
         </div>
       </Collapse>
-     
       <Footer />
     </>
   );
