1e3fc04e52384467b227a3f8013897f6bf30618f
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-05-14 18:44:41 -0400

    feature: display warning msg if user denied location access

--- frontend/src/views/AvailableNFT.jsx
+++ frontend/src/views/AvailableNFT.jsx
@@ -23,39 +23,47 @@ export default function AvailableNFT({
     <>
       <Navigation account={account} web3Handler={web3Handler} />
       <Container md="auto">
-        <Row>
-          <Col lg={9}>
-            {availableNft ? (
-              <Card className="m-4 text-center">
-                <Card.Img
-                  style={{ height: 580 }}
-                  variant="top"
-                  src={availableNft.image}
-                />
-                <Card.Body
-                  style={{
-                    background: "linear-gradient(#B2FBED, #9198e5)",
-                  }}>
-                  <Card.Title>{availableNft.collection}</Card.Title>
-                  <Card.Text>
-                    <small bg="primary">Price: {availableNft.price} ETH</small>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            ) : (
-              <div style={{ padding: "1rem 0" }}>
-                <h2>No NFT available at your location</h2>
-              </div>
-            )}
-          </Col>
-          <Col lg={3} className="fw-bold m-4 d-grid gap-2 col-3 mx-auto ">
-            <Buy
-              buyStoreItem={buyStoreItem}
-              item={availableNft}
-              purchases={purchases}
-            />
-          </Col>
-        </Row>
+        {location.allowLocation ? (
+          <Row>
+            <Col lg={9}>
+              {availableNft ? (
+                <Card className="m-4 text-center">
+                  <Card.Img
+                    style={{ height: 580 }}
+                    variant="top"
+                    src={availableNft.image}
+                  />
+                  <Card.Body
+                    style={{
+                      background: "linear-gradient(#B2FBED, #9198e5)",
+                    }}>
+                    <Card.Title>{availableNft.collection}</Card.Title>
+                    <Card.Text>
+                      <small bg="primary">
+                        Price: {availableNft.price} ETH
+                      </small>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              ) : (
+                <div style={{ padding: "1rem 0" }}>
+                  <h2>No NFT available at your location</h2>
+                </div>
+              )}
+            </Col>
+            <Col lg={3} className="fw-bold m-4 d-grid gap-2 col-3 mx-auto ">
+              <Buy
+                buyStoreItem={buyStoreItem}
+                item={availableNft}
+                purchases={purchases}
+              />
+            </Col>
+          </Row>
+        ) : (
+          <Row style={{ padding: 40, fontWeight: "bold", fontSize: 25 }}>
+            Please enable location to access this page
+          </Row>
+        )}
       </Container>
       <Footer />
     </>
