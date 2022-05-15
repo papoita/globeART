ce8361995201c74d774d5ce818e426f65cf691bf
Martha Staus <mjstaus@gmail.com>
2022-05-15 08:22:02 -0700

    FEATURE: Add Vancouver image placeholder for Modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -8,12 +8,12 @@ export default function Modal() {
         <div className="card w-96 bg-base-100 shadow-xl image-full z-50">
           <figure>
             <img
-              src="https://api.lorem.space/image/shoes?w=400&h=225"
-              alt="Shoes"
+              src="./images/vancouver.png"
+              alt="Vancouver"
             />
           </figure>
           <div className="card-body flex flex-col justify-between">
-            <h2 className="card-title">NFT Name</h2>
+            <h2 className="card-title">Vancouver</h2>
             <div className="card-actions align-self-end justify-end">
               <button className="btn btn-primary">Buy Now</button>
             </div>
