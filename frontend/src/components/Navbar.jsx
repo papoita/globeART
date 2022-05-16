9270093d85bf42353321e0f38cddb66d1624704a
Martha Staus <mjstaus@gmail.com>
2022-05-16 07:50:29 -0700

    Add links for home, usercollection to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     <>
       <div className="navbar bg-transparent">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">
+          <a href="/" className="btn btn-ghost normal-case text-xl">
             <img className="m-2" src="pig-logo.png"></img>
             <p className="font-shrikhand text-3xl text-slate-100">Trotter</p>
           </a>
@@ -56,7 +56,7 @@ export default function Navbar() {
                   </a>
                 </li>
                 <li>
-                  <a className="justify-between">My Collection</a>
+                  <a href="/mycollection" className="justify-between">My Collection</a>
                 </li>
                 <li>
                   <button onClick={() => disconnectWallet()}>
