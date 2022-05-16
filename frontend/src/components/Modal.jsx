455f178d97706adefd26912d3334e4948972a93a
Martha Staus <mjstaus@gmail.com>
2022-05-16 12:26:18 -0700

    feature: dynamically render nft data in modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-export default function Modal({ handleHideModal }) {
+export default function Modal({ handleHideModal, nft}) {
+
+  console.log(nft)
 
   return (
     <>
       <div className="fixed inset-0 h-full z-40 flex justify-center items-center">
         <div className="card w-96 bg-base-100 shadow-xl image-full">
           <figure>
-            <img src="./images/vancouver.png" alt="Vancouver" />
+            <img src={`./images/${nft.name}.png`} alt={nft.name} />
           </figure>
           <div className="card-body flex flex-col justify-between">
-            <h2 className="card-title">Vancouver</h2>
+            <h2 className="card-title">{nft.name}</h2>
           <button
             className="btn btn-sm btn-circle absolute right-2 top-2"
             onClick={() => handleHideModal()}
