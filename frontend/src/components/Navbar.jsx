0cdcfb2ccfeb49ba586cf0e1cdcf36a6be1c4d67
Martha Staus <mjstaus@gmail.com>
2022-05-23 08:20:53 -0700

    style: add hover animation to logo, adjust jdenticon size, adjust dropdown menu background and font color

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,9 +7,10 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="navbar bg-transparent">
-        <div className="flex-1">
-          <a href="/" className="btn btn-ghost normal-case text-xl">
+      <div className="navbar bg-transparent flex justify-between">
+        <div className="ml-3">
+          <a href="/" className="transition ease-in-out duration-300 hover:scale-105 normal-case text-xl
+          flex justify-start items-center">
             <img className="m-2" src="pig-logo.png"></img>
             <p className="font-shrikhand text-3xl text-slate-100">Trotter</p>
           </a>
@@ -27,28 +28,28 @@ export default function Navbar() {
           <div className="flex-none m-4">
             <div className="dropdown dropdown-end">
               
-              <button className="btn btn-ghost btn-circle flex justify-center mr-3">
-                <Jdenticon size="50" value={account} /> 
+              <button className="flex justify-center mr-3 transition ease-in-out duration-300 hover:scale-110">
+                <Jdenticon size="56" value={account} /> 
               </button>        
               <ul
                 tabIndex="0"
-                className="menu menu-compact dropdown-content p-2 shadow bg-base-100 rounded-box w-52"
+                className="menu mt-3 dropdown-content p-2 rounded-box w-52 menu-normal bg-gradient-to-r from-white  to-blue-500"
               >
                 <li>
                   <a
                     href={`https://etherscan.io/address/${account}`}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="justify-between inline-block"
+                    className="justify-between inline-block text-black"
                   >
                     Connected: <strong>{account.slice(0, 5) + "..." + account.slice(38, 42)}</strong>
                   </a>
                 </li>
                 <li>
-                  <a href="/mycollection" className="justify-between">My Collection</a>
+                  <a href="/mycollection" className="justify-between text-black">My Collection</a>
                 </li>
                 <li>
-                  <button onClick={() => disconnectWallet()}>
+                  <button className="text-black" onClick={() => disconnectWallet()}>
                     Disconnect Wallet
                   </button>
                 </li>
