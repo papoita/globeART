e350c058f8723cf24bd9f1e02d672be32f91b76f
Martha Staus <mjstaus@gmail.com>
2022-05-24 16:48:20 -0700

    feature: add useRef for capturing markers value

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import "../App.css";
 import Globe from "../components/globe";
 import Modal from "../components/Modal";
 
+import { getMarkers } from "../helpers/getMarkers";
+
 
 function Home() {
+  
   const [showModal, setShowModal] = useState(false);
   const [nft, setNft] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const markers = useRef(null);
+
+  useEffect(() => {
+    (async function asyncHandler() {
+      try {
+        markers.current = await getMarkers();
+        console.log("HOME - MARKERS", markers);
+        if (markers) setIsLoaded(true);
+      } catch (error) {
+        console.log(error);
+        setIsLoaded(false);
+      }
+    })();
+  }, []);
 
 
   const handleShowModal = (d) => {
@@ -20,7 +39,15 @@ function Home() {
 
   return (
     <>
-        {<Globe handleShowModal={handleShowModal} />}
+      {!isLoaded && (
+        <div className="flex justify-center items-center h-screen">
+          <img src="pig-spinner.png" className="animate-spin-slow"></img>
+        </div>
+      )}
+  
+      {isLoaded &&
+        <Globe handleShowModal={handleShowModal} markers={markers.current}/>
+      }
       {showModal && <Modal handleHideModal={handleHideModal} nft={nft} />}
     </>
   );
