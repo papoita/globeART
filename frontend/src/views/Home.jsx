a6a22b19c52a2cb1626dce44c50998cb3852034a
Martha Staus <mjstaus@gmail.com>
2022-05-24 12:51:34 -0700

    refactor: move markers logic from Home to Globe component

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,31 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import "../App.css";
 import Globe from "../components/globe";
-import Navbar from "../components/Navbar";
 import Modal from "../components/Modal";
 
-import { getMarkers } from "../helpers/getMarkers";
 
 function Home() {
   const [showModal, setShowModal] = useState(false);
   const [nft, setNft] = useState({});
-  const [isLoaded, setIsLoaded] = useState(false);
 
-  let markers;
-
-  useEffect(() => {
-    (async function asyncHandler() {
-      try {
-        markers = await getMarkers();
-        console.log("HOME- MARKERS", markers);
-        if (markers) setIsLoaded(true);
-      } catch (error) {
-        console.log(error);
-        setIsLoaded(false);
-      }
-    })();
-  }, []);
 
   const handleShowModal = (d) => {
     setShowModal(true);
@@ -38,7 +21,7 @@ function Home() {
   return (
     <>
       <div className="flex justify-center">
-        {<Globe handleShowModal={handleShowModal} markers={markers} />}
+        {<Globe handleShowModal={handleShowModal} />}
       </div>
       {showModal && <Modal handleHideModal={handleHideModal} nft={nft} />}
     </>
