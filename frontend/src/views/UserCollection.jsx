264637580d4fb06df6e8d871e1368f2ab4790d06
Martha Staus <mjstaus@gmail.com>
2022-05-24 18:10:17 -0700

    feature: UserCollection view renders account purchases

--- frontend/src/views/UserCollection.jsx
+++ frontend/src/views/UserCollection.jsx
@@ -1,8 +1,23 @@
-import useLoading from "../hooks/useLoading"
+import { useRef, useEffect } from "react";
+import useLoading from "../hooks/useLoading";
+import loadPurchasedItems from "../helpers/loadPurchasedItems";
 
-export default function UserCollection({ purchases }) {
-
+export default function UserCollection({ account}) {
   const { isLoaded, setIsLoaded } = useLoading();
+
+  const purchases = useRef(null);
+
+  useEffect(() => {
+    (async function asyncHandler() {
+      try {
+        purchases.current = await loadPurchasedItems(account);
+        setIsLoaded(true);
+      } catch (error) {
+        console.log(error);
+        setIsLoaded(false);
+      }
+    })();
+  }, []);
 
   return (
     <>
@@ -12,37 +27,39 @@ export default function UserCollection({ purchases }) {
         </div>
 
         {!isLoaded && (
-        <div className="flex justify-center items-center h-screen">
-          <img src="pig-spinner.png" className="animate-spin-slow"></img>
-        </div>
-      )}
+          <div className="flex justify-center items-center h-screen">
+            <img src="pig-spinner.png" className="animate-spin-slow"></img>
+          </div>
+        )}
 
-        <div className="container max-w-l m-auto flex flex-wrap flex-col md:flex-row justify-center">
-          {purchases.length > 0 ? (
-            purchases.map((item) => (
-              <div className="card card-compact bg-base-100 shadow-xl m-5 w-72">
-                <figure>
-                  <img src={`./images/${item.name}.png`} alt={item.name} />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{item.name}</h2>
-                  <p>Purchased for: 1 ETH </p>
-                  <p>Date purchased:  </p>
+        {isLoaded && (
+          <div className="container max-w-l m-auto flex flex-wrap flex-col md:flex-row justify-center">
+            {purchases.length > 0 ? (
+              purchases.map((item) => (
+                <div className="card card-compact bg-base-100 shadow-xl m-5 w-72">
+                  <figure>
+                    <img src={`./images/${item.name}.png`} alt={item.name} />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{item.name}</h2>
+                    <p>Purchased for: 1 ETH </p>
+                    <p>Date purchased: </p>
+                  </div>
                 </div>
+              ))
+            ) : (
+              <div className="flex flex-col my-3">
+                <p className="text-2xl">
+                  {" "}
+                  You don't have any items in your collection 😱{" "}
+                </p>
+                <a href="/" className="btn btn-primary mt-5 self-center">
+                  Go Back
+                </a>
               </div>
-            ))
-          ) : (
-            <div className="flex flex-col my-3">
-              <p className="text-2xl">
-                {" "}
-                You don't have any items in your collection 😱{" "}
-              </p>
-              <a href="/" className="btn btn-primary mt-5 self-center">
-                Go Back
-              </a>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        )}
       </div>
     </>
   );
