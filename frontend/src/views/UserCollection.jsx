3f097cf74ecc761a5b2d824e4e02f2ec03293c77
Martha Staus <mjstaus@gmail.com>
2022-05-24 18:19:43 -0700

    feature: add dynamic item price, add key for each child in list

--- frontend/src/views/UserCollection.jsx
+++ frontend/src/views/UserCollection.jsx
@@ -35,15 +35,14 @@ export default function UserCollection({ account}) {
         {isLoaded && (
           <div className="container max-w-l m-auto flex flex-wrap flex-col md:flex-row justify-center">
             {purchases.current.length > 0 ? (
-              purchases.current.map((item) => (
-                <div className="card card-compact bg-base-100 shadow-xl m-5 w-72">
+              purchases.current.map((item, idx) => (
+                <div key={idx} className="card card-compact bg-base-100 shadow-xl m-5 w-72">
                   <figure>
                     <img src={item.image} alt={item.name} />
                   </figure>
                   <div className="card-body">
                     <h2 className="card-title">{item.name}</h2>
-                    <p>Purchased for: 1 ETH </p>
-                    <p>Date purchased: </p>
+                    <p>Purchased for: {item.price} ETH</p>
                   </div>
                 </div>
               ))
