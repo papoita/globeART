76d6a5e762d73bb14ecc6f8b34b5a7756e50efe0
Martha Staus <mjstaus@gmail.com>
2022-05-26 06:43:05 -0700

    feature: Add RainbowKit button in Navbar, remove all other connect to wallet functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,7 @@
-import Jdenticon from "react-jdenticon";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-export default function Navbar({account, setAccount}) {
-
-  async function connectWallet() {
-    const results = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    setAccount(results[0]);
-    
-    window.ethereum.on("accountsChanged", async function (accounts) {
-      await connectWallet();
-    });
-  };
-
-  function disconnectWallet() {
-    setAccount(null);
-  };
-
+export default function Navbar() {
+ 
   return (
     <>
       <div className="navbar bg-transparent flex justify-between absolute h-20">
@@ -31,59 +16,7 @@ export default function Navbar({account, setAccount}) {
           </a>
         </div>
 
-        {!account && (
-          <div className="flex-none m-4 ">
-            <button className="btn" onClick={() => connectWallet()}>
-              Connect Wallet
-            </button>
-          </div>
-        )}
-
-        {account && (
-          <div className="flex-none m-4">
-            <div className="dropdown dropdown-end">
-              <button className="btn btn-circle btn-lg bg-transparent outline-none flex justify-center mr-3">
-                <div className="brightness-200 saturate-50">
-                  <Jdenticon size="50" value={account} />
-                </div>
-              </button>
-              <ul
-                tabIndex="0"
-                className="menu mt-3 dropdown-content p-2 rounded-box w-52 menu-normal bg-gradient-to-r from-cyan-500 to-blue-200"
-              >
-                <li>
-                  <a
-                    href={`https://etherscan.io/address/${account}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="justify-between inline-block text-black hover:bg-white hover:bg-opacity-20"
-                  >
-                    Connected:{" "}
-                    <strong>
-                      {account.slice(0, 5) + "..." + account.slice(38, 42)}
-                    </strong>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/mycollection"
-                    className="justify-between text-black hover:bg-white hover:bg-opacity-20"
-                  >
-                    My Collection
-                  </a>
-                </li>
-                <li>
-                  <button
-                    className="text-black hover:bg-white hover:bg-opacity-20"
-                    onClick={() => disconnectWallet()}
-                  >
-                    Disconnect Wallet
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        )}
+        <ConnectButton />
       </div>
     </>
   );
