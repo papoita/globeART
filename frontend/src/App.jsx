060ae0e25de316d2db43950bc8a11152c2d31b54
Martha Staus <mjstaus@gmail.com>
2022-05-26 06:42:37 -0700

    feature: RainbowKit setup in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -13,24 +15,44 @@ import UserCollection from "./views/UserCollection";
 
 // import useGeolocation from "./hooks/useGeolocation";
 
-export default function App() {
-  {/* const { location } = useGeolocation(); */}
+const { chains, provider } = configureChains(
+  [chain.polygon, chain.hardhat, chain.localhost],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+);
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
-  const [account, setAccount ] = useState(null);
+export default function App() {
+  {
+    /* const { location } = useGeolocation(); */
+  }
+
+  const [account, setAccount] = useState(null);
 
   return (
-    <Router>
-      <div className="bg-black w-full min-h-screen overflow-hidden">
-        <Navbar account={account} setAccount={setAccount}/>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route
-            path="/mycollection"
-            element={<UserCollection account={account}/>}
-          />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Router>
+          <div className="bg-black w-full min-h-screen overflow-hidden">
+            <Navbar account={account} setAccount={setAccount} />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route
+                path="/mycollection"
+                element={<UserCollection account={account} />}
+              />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
