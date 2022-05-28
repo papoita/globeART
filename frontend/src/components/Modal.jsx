d68eec508a0766d11ddbca385052e0d707c89f28
Martha Staus <mjstaus@gmail.com>
2022-05-28 07:48:25 -0700

    feature: Display sold out badge if item sold, Display BuyNow button if not sold

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import buyMarketplaceItem from "../helpers/buyMarketplaceItem";
 
-export default function Modal({ handleHideModal, nft}) {
-
+export default function Modal({ handleHideModal, nft }) {
   return (
     <>
       <div className="fixed inset-0 h-full z-40 flex justify-center items-center">
-        <div className="card w-96 bg-base-100 shadow-xl image-full transition ease-in-out duration-300 hover:scale-101">
+        <div className="card card-compact w-96 bg-base-100 shadow-xl transition ease-in-out duration-300 hover:scale-101">
           <figure>
-            <img src={`./images/thumbnails/${nft.name}.jpg`} alt={nft.name} />
+            <img
+              className="brightness-75"
+              src={`./images/thumbnails/${nft.name}.jpg`}
+              alt={nft.name}
+            />
           </figure>
-          <div className="card-body flex flex-col justify-between">
-            <h2 className="card-title font-urbanist text-white text-2xl">{nft.name}</h2>
-          <button
-            className="btn btn-sm btn-circle absolute right-2 top-2"
-            onClick={() => handleHideModal()}
-          >
-            ✕
-          </button>
-            <div className="card-actions align-self-end justify-end">
-              <button
-                className="btn btn-primary"
-                onClick={()=> buyMarketplaceItem(nft)}
-              >
-                Buy Now
-              </button>
-            </div>
+          <div className="card-body bg-white">
+            <h2 className="card-title font-urbanist text-black text-2xl">
+              {nft.name}
+              {nft.sold && <div class="badge badge-secondary">SOLD OUT</div>}
+            </h2>
+            <button
+              className="btn btn-sm btn-circle absolute right-2 top-2"
+              onClick={() => handleHideModal()}
+            >
+              ✕
+            </button>
+            {!nft.sold && (
+              <div className="card-actions align-self-end justify-end">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => buyMarketplaceItem(nft)}
+                >
+                  Buy Now
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
