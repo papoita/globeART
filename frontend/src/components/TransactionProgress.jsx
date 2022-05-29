5f2b2d55ce6fe6dfccf668bc3e33f947689ac1fe
Martha Staus <mjstaus@gmail.com>
2022-05-29 14:25:22 -0700

    feature: Add AnimatedPig component to TransactionProgress. Adjust styling

--- frontend/src/components/TransactionProgress.jsx
+++ frontend/src/components/TransactionProgress.jsx
@@ -1,8 +1,14 @@
+import AnimatedPig from "./AnimatedPig/AnimatedPig";
+
 export default function TransactionProgress() {
   return (
-    <div className="fixed inset-0 h-full z-50 flex flex-col justify-center items-center">
-      <progress class="progress w-56"></progress>
-      <p>Transaction in Progress</p>
+    <div className="z-50 bg-white opacity-70 absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
+      <div className="bg-black opacity-100 animate-bounce rounded-lg py-2 px-4">
+        <p className="text-white text-2xl font-bold">Transaction in Progress</p>
+      </div>
+      <div className="w-1/2">
+        <AnimatedPig />
+      </div>
     </div>
   );
 }
