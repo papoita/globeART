3554624495c8ca5211eb7a599e1d5e43e745a6bf
Martha Staus <mjstaus@gmail.com>
2022-05-31 08:24:51 -0700

    feature: add point on globe to show user's location (render underneath label to not interfere with labelclick)

--- frontend/src/components/globe.jsx
+++ frontend/src/components/globe.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from "react";
 import ReactGlobe from "react-globe.gl";
 
-export default function Globe({ handleShowModal, markers }) {
-
+export default function Globe({ handleShowModal, markers, userPoint }) {
   const globeEl = useRef();
+  console.log(userPoint)
 
   useEffect(() => {
     globeEl.current.controls().enableZoom = false;
@@ -19,18 +19,26 @@ export default function Globe({ handleShowModal, markers }) {
           globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
           bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
           backgroundColor="rgba(0,0,0,0)"
+          enableMarkerGlow={true}
           labelsData={markers}
+          labelAltitude={0.002}
           labelLat={(d) => d.lat}
           labelLng={(d) => d.lng}
           labelText={(d) => d.name}
           labelSize={1.2}
-          labelDotRadius={0.9}
+          labelDotRadius={0.6}
           labelColor={(d) => d.color}
           labelResolution={2}
           onLabelClick={(d) => handleShowModal(d)}
+          pointsData={userPoint}
+          pointLat={(d)=> d.lat}
+          pointLng={(d)=> d.lng}
+          pointColor={(d)=> d.color}
+          pointAltitude={0}
+          pointRadius={1}
           width={660}
         />
       </div>
     </>
   );
-};
+}
