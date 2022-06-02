e601df0ea7c424c004cd595ee8246185696910cd
Martha Staus <mjstaus@gmail.com>
2022-06-02 12:29:57 -0700

    feature: Modify Shop test to use Token contract

--- backend/test/Shop.js
+++ backend/test/Shop.js
@@ -6,7 +6,7 @@ const toWei = (num) => ethers.utils.parseEther(num.toString())
 const fromWei = (num) => ethers.utils.formatEther(num)
 
 describe("Shop", function () {
-  let nft;
+  let token;
   let Shop;
   let shop;
   const URI = "Test URI";
@@ -14,14 +14,14 @@ describe("Shop", function () {
 
   beforeEach(async function () {
     // Get the ContractFactories and Signers here.
-    const NFT = await ethers.getContractFactory("GlobeArtNFT");
+    const Token = await ethers.getContractFactory("Token");
     Shop = await ethers.getContractFactory("Shop");
     [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
     
 
     // To deploy contracts
-    nft = await NFT.deploy();
-    await nft.deployed();
+    token = await Token.deploy();
+    await token.deployed();
     shop = await Shop.deploy();
     await shop.deployed();
   });
@@ -29,30 +29,30 @@ describe("Shop", function () {
   describe("Making shop items", function () {
     let price = 1;
     beforeEach(async function () {
-      // deployer mints an nft
-      await nft.connect(deployer).createGlobeArtNFT(URI)
-      // deployer approves shop to spend nft
-      await nft.connect(deployer).setApprovalForAll(shop.address, true)
+      // deployer mints a token
+      await token.connect(deployer).mint(deployer.address, 1, 1)
+      // deployer approves shop to spend token
+      await token.connect(deployer).setApprovalForAll(shop.address, true)
     })
-    it("should track newly created item, transfer NFT from seller to shop and emit Offered event", async function () {
-      // deployer offers their nft at a price of 1 ether
-      await expect(shop.connect(deployer).makeItem(nft.address, 1 , collection, toWei(price)))
+    it("should track newly created item, transfer token from seller to shop and emit Offered event", async function () {
+      // deployer offers their token at a price of 1 ether
+      await expect(shop.connect(deployer).makeItem(token.address, 1 , collection, toWei(price)))
         .to.emit(shop, "Offered")
         .withArgs(
           1,
-          nft.address,
+          token.address,
           1,
           toWei(price),
           deployer.address
         )
-      // Owner of NFT should now be the shop
-      expect(await nft.ownerOf(1)).to.equal(shop.address);
+      // Owner of Token should now be the shop
+      expect(await token.balanceOf(shop.address, 1)).to.equal(1);
       // Item count should now equal 1
       expect(await shop.itemCount()).to.equal(1)
       // Get item from items mapping then check fields to ensure they are correct
       const item = await shop.items(1)
       expect(item.itemId).to.equal(1)
-      expect(item.nft).to.equal(nft.address)
+      expect(item.nft).to.equal(token.address)
       expect(item.tokenId).to.equal(1)
       expect(item.collection).to.equal(collection)
       expect(item.price).to.equal(toWei(price))
@@ -60,11 +60,11 @@ describe("Shop", function () {
     })
     it("should fail if called by account that is not the deployer", async function () {
       await expect(
-        shop.connect(addr1).makeItem(nft.address, 1, collection, price )).to.be.revertedWith("Ownable: caller is not the owner");
+        shop.connect(addr1).makeItem(token.address, 1, collection, price )).to.be.revertedWith("Ownable: caller is not the owner");
     });
     it("should fail if price is set to zero", async function () {
       await expect(
-        shop.connect(deployer).makeItem(nft.address, 1, collection, 0 )).to.be.revertedWith("Price must be greater than zero");
+        shop.connect(deployer).makeItem(token.address, 1, collection, 0 )).to.be.revertedWith("Price must be greater than zero");
     });
   });
 
@@ -72,14 +72,14 @@ describe("Shop", function () {
     let price = 2;
     let priceInWei;
     beforeEach(async function () {
-      // deployer mints an nft
-      await nft.connect(deployer).createGlobeArtNFT(URI)
+      // deployer mints an token
+      await token.connect(deployer).mint(deployer.address, 1, 1)
       // deployer approves shop to spend tokens
-      await nft.connect(deployer).setApprovalForAll(shop.address, true)
-      // deployer makes their nft a shop item.
-      await shop.connect(deployer).makeItem(nft.address, 1 , collection, toWei(price))
+      await token.connect(deployer).setApprovalForAll(shop.address, true)
+      // deployer makes their token a shop item.
+      await shop.connect(deployer).makeItem(token.address, 1 , collection, toWei(price))
     })
-    it("Should update item as sold, pay seller, transfer NFT to buyer, charge fees and emit a Bought event", async function () {
+    it("Should update item as sold, pay seller, transfer Token to buyer, charge fees and emit a Bought event", async function () {
       const sellerInitalEthBal = await deployer.getBalance()
       priceInWei = await shop.getPrice(1);
       // addr 2 purchases item.
@@ -87,7 +87,7 @@ describe("Shop", function () {
       .to.emit(shop, "Bought")
         .withArgs(
           1,
-          nft.address,
+          token.address,
           1,
           toWei(price),
           deployer.address,
@@ -96,10 +96,10 @@ describe("Shop", function () {
       const sellerFinalEthBal = await deployer.getBalance()
       // Item should be marked as sold
       expect((await shop.items(1)).sold).to.equal(true)
-      // Seller should receive payment for the price of the NFT sold.
+      // Seller should receive payment for the price of the Token sold.
       expect(+fromWei(sellerFinalEthBal)).to.equal(+price + +fromWei(sellerInitalEthBal))
-      // The buyer should now own the nft
-      expect(await nft.ownerOf(1)).to.equal(addr1.address);
+      // The buyer should now own the token
+      expect(await token.balanceOf(addr1.address, 1)).to.equal(1);
     })
     it("Should fail for invalid item ids, sold items and when not enough ether is paid", async function () {
       // fails for invalid item ids
