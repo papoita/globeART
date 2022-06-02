ef703333b9aa4b2ab245c4f49581c22fab04979a
Martha Staus <mjstaus@gmail.com>
2022-06-02 08:18:14 -0700

    feature: add public name and symbol to token.sol

--- backend/contracts/Token.sol
+++ backend/contracts/Token.sol
@@ -2,27 +2,35 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 contract Token is ERC1155, Ownable, ERC1155Burnable {
-    constructor() ERC1155("") {}
+
+    string public name;
+    string public symbol;
+
+    constructor() ERC1155("http://URI/{id}.com") {
+        name = "Trotter";
+        symbol = "TRTR";
+    }
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+    function mint(address account, uint256 id, uint256 amount)
         public
         onlyOwner
     {
-        _mint(account, id, amount, data);
+        _mint(account, id, amount, "");
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts)
         public
         onlyOwner
     {
-        _mintBatch(to, ids, amounts, data);
+        _mintBatch(to, ids, amounts, "");
     }
 }
