4d69460a2eb5086d3696ea32b00fa6ec2b378b23
Martha Staus <mjstaus@gmail.com>
2022-06-04 10:07:41 -0700

    feature: Add timeout for showing modal on labelClick to allow time for globe to spin to location before modal opens

--- frontend/src/components/globe.jsx
+++ frontend/src/components/globe.jsx
@@ -4,10 +4,19 @@ import ReactGlobe from "react-globe.gl";
 
 const MOBILE_BREAKPOINT = 768;
 
-export default function Globe({ globeEl, handleShowModal, markers, userLocation }) {
+export default function Globe({
+  globeEl,
+  handleShowModal,
+  markers,
+  userLocation,
+}) {
   let isMobile = window.outerWidth <= MOBILE_BREAKPOINT;
-  const [height, setHeight] = useState(isMobile ? window.outerHeight : window.outerWidth * .6);
-  const [width, setWidth] = useState(isMobile ? window.outerHeight * .55 : window.outerWidth);
+  const [height, setHeight] = useState(
+    isMobile ? window.outerHeight : window.outerWidth * 0.6
+  );
+  const [width, setWidth] = useState(
+    isMobile ? window.outerHeight * 0.55 : window.outerWidth
+  );
   const userLat = userLocation.coordinates.lat;
   const userLon = userLocation.coordinates.lon;
 
@@ -23,20 +32,25 @@ export default function Globe({ globeEl, handleShowModal, markers, userLocation 
   const ROTATION_SPEED = 500;
 
   const handleOnLabelClick = (d) => {
-    globeEl.current.pointOfView({ lat: d.lat, lng: d.lng, altitude: 1 }, ROTATION_SPEED);
-    handleShowModal(d);
-  }
+    globeEl.current.pointOfView(
+      { lat: d.lat, lng: d.lng, altitude: 1 },
+      ROTATION_SPEED
+    );
+    setTimeout(() => {
+      handleShowModal(d);
+    }, 200);
+  };
 
   const handleOnGlobeReady = () => {
     globeEl.current.controls().enableZoom = false;
     globeEl.current.controls().autoRotate = true;
-    globeEl.current.controls().autoRotateSpeed = 0.1;          
-    globeEl.current.pointOfView(MAP_CENTER, ROTATION_SPEED)    
-  }
+    globeEl.current.controls().autoRotateSpeed = 0.1;
+    globeEl.current.pointOfView(MAP_CENTER, ROTATION_SPEED);
+  };
 
   useEffect(() => {
-    window.addEventListener('resize', () => {
-      console.log('isMobile', isMobile)
+    window.addEventListener("resize", () => {
+      console.log("isMobile", isMobile);
       if (width <= MOBILE_BREAKPOINT) {
         isMobile = true;
       } else if (width > MOBILE_BREAKPOINT) {
@@ -46,11 +60,10 @@ export default function Globe({ globeEl, handleShowModal, markers, userLocation 
         setHeight(window.outerHeight);
         setWidth(window.outerHeight);
       } else {
-        setHeight(window.outerWidth * .6);
+        setHeight(window.outerWidth * 0.6);
         setWidth(window.outerWidth);
       }
     });
-
   }, [height, width]);
 
   return (
@@ -74,9 +87,9 @@ export default function Globe({ globeEl, handleShowModal, markers, userLocation 
           onLabelClick={(d) => handleOnLabelClick(d)}
           onGlobeReady={handleOnGlobeReady}
           pointsData={userPoint}
-          pointLat={(d)=> d.lat}
-          pointLng={(d)=> d.lng}
-          pointColor={(d)=> d.color}
+          pointLat={(d) => d.lat}
+          pointLng={(d) => d.lng}
+          pointColor={(d) => d.color}
           pointAltitude={0}
           pointRadius={1}
           height={height}
