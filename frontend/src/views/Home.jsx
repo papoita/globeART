0ccc96591db77cd6969cf1b7b7e75351f47e3102
Martha Staus <mjstaus@gmail.com>
2022-06-04 07:57:42 -0700

    refactor: Hoist globeEl to Home component

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from "react";
-
 import "../App.css";
 import Globe from "../components/globe";
 import Modal from "../components/Modal";
@@ -7,35 +6,38 @@ import Modal from "../components/Modal";
 import useLoading from "../hooks/useLoading";
 import useGeolocation from "../hooks/useGeolocation";
 import { getMarkers } from "../helpers/getMarkers";
-import { renderMatches } from "react-router-dom";
 
 function Home() {
+  const globeEl = useRef();
   const { location } = useGeolocation();
   const [showModal, setShowModal] = useState(false);
   const [nft, setNft] = useState({});
 
   const { isLoaded, setIsLoaded } = useLoading();
-
   const markers = useRef(null);
 
   useEffect(() => {
     (async function asyncHandler() {
       try {
         markers.current = await getMarkers();
-        if (markers) setIsLoaded(true);
+        if (markers && location?.coordinates?.lat) setIsLoaded(true);
       } catch (error) {
         console.log(error);
         setIsLoaded(false);
       }
     })();
-  }, [isLoaded]);
+  }, [isLoaded, location]);
 
   const handleShowModal = (d) => {
     setShowModal(true);
     setNft(d);
   };
+
   const handleHideModal = () => {
+    const { lat, lng } = globeEl.current.pointOfView();
+    const ROTATION_SPEED = 250;
     setShowModal(false);
+    globeEl.current.pointOfView({ lat, lng, altitude: 2.5 }, ROTATION_SPEED);
   };
 
   return (
@@ -51,9 +53,21 @@ function Home() {
       )}
 
       {isLoaded && (
-        <Globe handleShowModal={handleShowModal} markers={markers.current} userLocation={location} />
+        <Globe
+          globeEl={globeEl}
+          handleShowModal={handleShowModal}
+          markers={markers.current}
+          userLocation={location}
+        />
       )}
-      {showModal && <Modal handleHideModal={handleHideModal} nft={nft} userLocation={location.city} />}
+
+      {showModal && (
+        <Modal
+          handleHideModal={handleHideModal}
+          nft={nft}
+          userLocation={location.city}
+        />
+      )}
     </>
   );
 }
