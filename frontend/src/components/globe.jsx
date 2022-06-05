b95de5221c5b278cf9224d6717b5100ec0783d93
Martha Staus <mjstaus@gmail.com>
2022-06-05 08:18:11 -0700

    feature: Add stars custom layer to globe - blinking stars (adjusting opacity) in progress

--- frontend/src/components/globe.jsx
+++ frontend/src/components/globe.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { defaultDotMarkerOptions } from "react-globe.gl";
 import ReactGlobe from "react-globe.gl";
+import * as THREE from "three";
 
 const MOBILE_BREAKPOINT = 768;
 
@@ -31,6 +31,17 @@ export default function Globe({
   const MAP_CENTER = { lat: userLat, lng: userLon, altitude: 2.5 };
   const ROTATION_SPEED = 500;
 
+  const N = 300;
+  const stars = [...Array(N).keys()].map(() => ({
+    // opacity: Math.random() + 0.1,
+    lat: (Math.random() - 0.5) * 180,
+    lng: (Math.random() - 0.5) * 360,
+    alt: Math.random() * 2 + 5,
+    radius: Math.random() * 1.1,
+    color: `rgba(255,255,255,1)`
+  }));
+  // const [starOpacity, setStarOpacity] = useState(stars);
+  
   const handleOnLabelClick = (d) => {
     globeEl.current.pointOfView(
       { lat: d.lat, lng: d.lng, altitude: 1 },
@@ -47,6 +58,7 @@ export default function Globe({
     globeEl.current.controls().autoRotateSpeed = 0.1;
     globeEl.current.pointOfView(MAP_CENTER, ROTATION_SPEED);
   };
+
 
   useEffect(() => {
     window.addEventListener("resize", () => {
@@ -66,15 +78,40 @@ export default function Globe({
     });
   }, [height, width]);
 
+  // useEffect(() => {
+  //   (function blinkStars() {
+  //     starOpacity.forEach(d => {
+  //       d.opacity === 1 ? d.opacity = 0 : d.opacity += 0.1
+  //     });
+  //     setStarOpacity(starOpacity.slice());
+  //     requestAnimationFrame(blinkStars);
+  //   })();
+  // }, [])
+
+
   return (
     <>
       <div className="flex justify-center h-screen z-30">
         <ReactGlobe
           ref={globeEl}
-          globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+          atmosphereAltitude={0.2}
           bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-          backgroundColor="rgba(0,0,0,0)"
+          backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+          customLayerData={stars}
+          customThreeObject={(d) =>
+            new THREE.Mesh(
+              new THREE.SphereBufferGeometry(d.radius),
+              new THREE.MeshLambertMaterial({ color: d.color})
+            )
+          }
+          customThreeObjectUpdate={(obj, d) => {
+            Object.assign(
+              obj.position,
+              globeEl.current.getCoords(d.lat, d.lng, d.alt)
+            );
+          }}
           enableMarkerGlow={true}
+          globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
           labelsData={markers}
           labelAltitude={0.002}
           labelLat={(d) => d.lat}
@@ -92,6 +129,7 @@ export default function Globe({
           pointColor={(d) => d.color}
           pointAltitude={0}
           pointRadius={1}
+          showAtmosphere={true}
           height={height}
           width={width}
         />
