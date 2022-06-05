911d449d09eec113b263b059cee34d002039fd59
Martha Staus <mjstaus@gmail.com>
2022-06-05 10:10:56 -0700

    feature: Add button to center globe on user's location

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -43,6 +43,16 @@ function Home() {
     }, 200);
   };
 
+  const centerOnLocation = () => {
+    const MAP_CENTER = {
+      lat: location?.coordinates?.lat,
+      lng: location?.coordinates?.lon,
+      altitude: 2.5,
+    };
+    const ROTATION_SPEED = 500;
+    globeEl.current.pointOfView(MAP_CENTER, ROTATION_SPEED);
+  };
+
   return (
     <>
       {!isLoaded && (
@@ -56,12 +66,38 @@ function Home() {
       )}
 
       {isLoaded && (
-        <Globe
-          globeEl={globeEl}
-          handleShowModal={handleShowModal}
-          markers={markers.current}
-          userLocation={location}
-        />
+        <>
+          <Globe
+            globeEl={globeEl}
+            handleShowModal={handleShowModal}
+            markers={markers.current}
+            userLocation={location}
+          />
+          <button
+            class="btn btn-round absolute bottom-0 right-0 m-3 mr-9"
+            onClick={centerOnLocation}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </button>
+        </>
       )}
       <Transition
         show={showModal}
