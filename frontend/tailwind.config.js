2411bafd6aedb5afa2cb9279999eb71d22577288
Martha Staus <mjstaus@gmail.com>
2022-06-18 09:08:52 -0700

    chore: reorganize theme extensions

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -7,23 +6,26 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'shrikhand': ['shrikhand', 'cursive'],
-        'urbanist': ['urbanist', 'sans-serif']
-      },
       animation: {
-        'spin-slow': 'spin 2.3s linear infinite',
+        "spin-slow": "spin 2.3s linear infinite",
+      },
+      fontFamily: {
+        shrikhand: ["shrikhand", "cursive"],
+        urbanist: ["urbanist", "sans-serif"],
       },
       scale: {
-        '10003': '1.003',
-        '102': '1.02',
+        10003: "1.003",
+        102: "1.02",
       },
       boxShadow: {
-        'custom-sm': '1px 2px 2px hsl(220deg 60% 50% / 0.333),2px 4px 4px hsl(220deg 60% 50% / 0.333), 3px 6px 6px hsl(220deg 60% 50% / 0.333)',
-        'custom-med': '1px 2px 2px hsl(220deg 60% 50% / 0.333),2px 4px 4px hsl(220deg 60% 50% / 0.333), 4px 8px 8px hsl(220deg 60% 50% / 0.333), 8px 16px 16px hsl(220deg 60% 50% / 0.333)',
-        'custom-lg': '1px 2px 2px hsl(220deg 60% 50% / 0.2), 2px 4px 4px hsl(220deg 60% 50% / 0.2), 4px 8px 8px hsl(220deg 60% 50% / 0.2), 8px 16px 16px hsl(220deg 60% 50% / 0.2),16px 32px 32px hsl(220deg 60% 50% / 0.2)',
-      }
+        "custom-sm":
+          "1px 2px 2px hsl(220deg 60% 50% / 0.333),2px 4px 4px hsl(220deg 60% 50% / 0.333), 3px 6px 6px hsl(220deg 60% 50% / 0.333)",
+        "custom-med":
+          "1px 2px 2px hsl(220deg 60% 50% / 0.333),2px 4px 4px hsl(220deg 60% 50% / 0.333), 4px 8px 8px hsl(220deg 60% 50% / 0.333), 8px 16px 16px hsl(220deg 60% 50% / 0.333)",
+        "custom-lg":
+          "1px 2px 2px hsl(220deg 60% 50% / 0.2), 2px 4px 4px hsl(220deg 60% 50% / 0.2), 4px 8px 8px hsl(220deg 60% 50% / 0.2), 8px 16px 16px hsl(220deg 60% 50% / 0.2),16px 32px 32px hsl(220deg 60% 50% / 0.2)",
+      },
     },
   },
   plugins: [require("daisyui")],
-}
+};
