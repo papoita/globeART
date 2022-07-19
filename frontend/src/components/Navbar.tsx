4b7769f4dcb61f46368a3a75b7b4ebaec6089e77
Martha Staus <mjstaus@gmail.com>
2022-07-19 13:56:53 -0700

    stye: show chain icon in navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -66,7 +66,7 @@ export default function Navbar({location, globeEl}: NavbarProps) {
               smallScreen: "avatar",
               largeScreen: "full",
             }}
-            chainStatus="none"
+            chainStatus={"icon"}
             showBalance={false}
           />
         </div>
